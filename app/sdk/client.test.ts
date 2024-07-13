eb37f35010247b9dc8b587cd037ddddf1ee5f37f
Wijnand Karsens <wijnand@karsens.com>
2024-07-13 16:14:00 +0200

    improvements

--- app/sdk/client.test.ts
+++ app/sdk/client.test.ts
@@ -1,3 +1,4 @@
 import * as client from "./client.js";
 
-client.migrateAgentAdmin("read", {}).then((result) => console.log(result));
+//client.migrateAgentAdmin("read", {}).then((result) => console.log(result));
+client.migrateAgentOpenapi("read", {}).then((result) => console.log(result));
